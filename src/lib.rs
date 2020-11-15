81b0346aad6e51afe0f84200158a32d3510c22e8
David Spies <dnspies@gmail.com>
2020-11-15 09:28:49 -0800

    More solid conceptual model

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use differential_dataflow::{input, Collection};
 use std::collections::hash_map::{Entry, HashMap};
 use std::fmt::Debug;
 use std::hash::Hash;
+use std::marker::PhantomData;
 use std::sync::{Arc, Mutex, RwLock, RwLockReadGuard};
 use timely::communication::{Allocator, WorkerGuards};
 use timely::dataflow::operators::probe::Handle;
@@ -17,18 +18,17 @@ pub fn execute_from_args<I, T1, T2: Send + Sync, F, G>(
 where
     I: Iterator<Item = String>,
     F: Fn(&mut Child<Worker<Allocator>, usize>, &mut InputRegister) -> T1 + Send + Sync + 'static,
-    G: Fn(Context, T1) -> T2 + Send + Sync + 'static,
+    G: Fn(&mut Context, T1) -> T2 + Send + Sync + 'static,
 {
     timely::execute_from_args(iter, move |worker| {
         let mut register = InputRegister::new();
         let structures = worker.dataflow(|scope| setup(scope, &mut register));
         let mut context = Context {
-            worker,
-            register,
+            worker: Mutex::new(worker),
             current_step: 0,
+            register,
         };
-        context.commit();
-        execute(context, structures)
+        execute(&mut context, structures)
     })
 }
 
@@ -56,7 +56,7 @@ trait Registerable {
     fn advance_to(&mut self, t: usize);
 }
 
-impl<D: Clone + Ord + Debug + 'static, R: Semigroup> Registerable for InputSessionInner<D, R> {
+impl<D: Clone + Ord + Debug, R: Semigroup> Registerable for InputSessionInner<D, R> {
     fn advance_to(&mut self, t: usize) {
         self.0.advance_to(t);
         self.0.flush();
@@ -67,55 +67,37 @@ pub struct InputSession<D: Clone + Ord + Debug + 'static, R: Semigroup>(
     Arc<Mutex<InputSessionInner<D, R>>>,
 );
 
-impl<D: Clone + Ord + Debug + 'static, R: Semigroup> InputSession<D, R> {
-    fn with_inner<T, F: FnOnce(&mut input::InputSession<usize, D, R>) -> T>(&mut self, f: F) -> T {
+impl<D: Clone + Ord + Debug, R: Semigroup> InputSession<D, R> {
+    fn with_inner<T, F: FnOnce(&mut input::InputSession<usize, D, R>) -> T>(&self, f: F) -> T {
         f(&mut self.0.lock().unwrap().0)
     }
-    pub fn to_collection<G: Scope<Timestamp = usize>>(
-        &mut self,
-        scope: &mut G,
-    ) -> Collection<G, D, R> {
+    pub fn to_collection<G: Scope<Timestamp = usize>>(&self, scope: &mut G) -> Collection<G, D, R> {
         self.with_inner(|i| i.to_collection(scope))
     }
-    pub fn update(&mut self, element: D, change: R) {
+    pub fn update(&self, _: &mut ContextInput, element: D, change: R) {
         self.with_inner(|i| i.update(element, change))
     }
 }
 
-impl<D: Clone + Ord + Debug + 'static> InputSession<D, isize> {
-    pub fn insert(&mut self, element: D) {
+impl<D: Clone + Ord + Debug> InputSession<D, isize> {
+    pub fn insert(&self, _: &mut ContextInput, element: D) {
         self.with_inner(|i| i.insert(element))
     }
 }
 
 pub struct Context<'a> {
-    worker: &'a mut Worker<Allocator>,
-    register: InputRegister,
+    worker: Mutex<&'a mut Worker<Allocator>>,
     current_step: usize,
+    register: InputRegister,
 }
 
+// We're pretending that the input data to be commited is stored in this ContextInput. Hence the dummy lifetimes.
+pub struct ContextInput<'b>(PhantomData<&'b ()>);
+pub struct ContextOutput<'a, 'b>(&'b Context<'a>);
+
 impl<'a> Context<'a> {
-    // Could potentially deadlock if another read was opened before calling commit and hasn't closed yet
-    fn read_deadlocking<'c, 'b: 'c, D, R>(
-        &mut self,
-        r: &'b ReadRef<HashMap<D, R>>,
-    ) -> RwLockReadGuard<'c, HashMap<D, R>> {
-        while r.handle.less_than(&self.current_step) {
-            self.worker.step();
-        }
-        r.data.read().unwrap()
-    }
-    /// Open a reference to a single collection output without creating a reader
-    pub fn read<'c, 'b: 'c, D, R>(
-        &'c mut self, // Although not necessary to compile, this lifetime annotation makes deadlock impossible since commit can't be called while the result is in scope
-        r: &'b ReadRef<HashMap<D, R>>,
-    ) -> RwLockReadGuard<'c, HashMap<D, R>> {
-        self.read_deadlocking(r)
-    }
-    /// Create a reader for opening references to multiple collection outputs simultaneously
-    pub fn get_reader<'c>(&'c mut self) -> Reader<'c, 'a> {
-        // For opening references to multiple collections at the same time
-        Reader(self)
+    pub fn get_io<'b>(&'b mut self) -> (ContextInput<'b>, ContextOutput<'a, 'b>) {
+        (ContextInput(PhantomData), ContextOutput(self))
     }
     pub fn commit(&mut self) {
         self.current_step += 1;
@@ -126,17 +108,6 @@ impl<'a> Context<'a> {
         for r in &mut reg_refs {
             r.advance_to(self.current_step);
         }
-    }
-}
-
-pub struct Reader<'c, 'a>(&'c mut Context<'a>);
-
-impl<'c, 'a> Reader<'c, 'a> {
-    pub fn read<'b: 'c, D, R>(
-        &mut self,
-        r: &'b ReadRef<HashMap<D, R>>,
-    ) -> RwLockReadGuard<'c, HashMap<D, R>> {
-        self.0.read_deadlocking(r)
     }
 }
 
@@ -164,6 +135,19 @@ impl<G: Scope<Timestamp = usize>, D: Data + Eq + Hash, R: Monoid> CreateOutput<D
 pub struct ReadRef<D> {
     data: Arc<RwLock<D>>,
     handle: Handle<usize>,
+}
+
+impl<T> ReadRef<T> {
+    pub fn read<'b>(
+        &'b self,
+        ContextOutput(context): &'b ContextOutput, // Although not necessary to compile, this lifetime annotation is important since it prevents deadlock by making sure the output gets cleaned up before the next commit call.
+    ) -> RwLockReadGuard<'b, T> {
+        let mut worker = context.worker.lock().unwrap();
+        while self.handle.less_than(&context.current_step) {
+            worker.step();
+        }
+        self.data.read().unwrap()
+    }
 }
 
 fn apply_update<D: Eq + Hash, R: Semigroup>(data: &mut HashMap<D, R>, k: D, v: R) {
