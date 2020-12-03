560fe67d7afeecc5d5042baae99b17857c11b901
David Spies <dnspies@gmail.com>
2020-12-03 08:49:10 -0800

    Convenience stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use differential_dataflow::difference::Semigroup;
 use differential_dataflow::Collection;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{hash_map, BTreeMap, HashMap};
 use std::fmt::Debug;
 use std::hash::Hash;
+use std::ops::Deref;
 use timely::dataflow::scopes::Scope;
 use timely::Data;
 
@@ -72,7 +73,7 @@ impl<G: Scope<Timestamp = usize>, D: Data, R: Default + Semigroup> CreateCountOu
 pub type ReadMapMapRef<K, V, R = isize> = ReadRef<HashMap<K, HashMap<V, R>>, (K, V), R>;
 
 pub trait CreateMapOutput<K, V, R = isize> {
-    fn create_map_output(&self) -> ReadMapMapRef<K, V, R>;
+    fn create_map_output(&self) -> DefaultedMap<K, V, R>;
 }
 impl<
         G: Scope<Timestamp = usize>,
@@ -81,10 +82,117 @@ impl<
         R: Default + Semigroup,
     > CreateMapOutput<K, V, R> for Collection<G, (K, V), R>
 {
-    fn create_map_output(&self) -> ReadMapMapRef<K, V, R> {
+    fn create_map_output(&self) -> DefaultedMap<K, V, R> {
         self.create_updater(|data, d, r| apply_map_update(data, d, SG(r)))
+            .with_default()
     }
 }
+
+pub struct DefaultedMap<K, V, R = isize>(ReadMapMapRef<K, V, R>);
+
+impl<K, V, R> ReadMapMapRef<K, V, R> {
+    pub fn with_default(self) -> DefaultedMap<K, V, R> {
+        DefaultedMap(self)
+    }
+}
+
+impl<K, V, R> DefaultedMap<K, V, R> {
+    pub fn read<'a>(&'a self, context: &'a ContextOutput) -> DefaultedRef<'a, K, V, R> {
+        DefaultedRef(self.0.read(context))
+    }
+}
+
+pub struct DefaultedRef<'a, K, V, R = isize>(ReadRefRef<'a, HashMap<K, HashMap<V, R>>, (K, V), R>);
+
+pub enum MaybeBorrowed<'a, T> {
+    Owned(T),
+    Borrowed(&'a T),
+}
+
+impl<'a, T> Deref for MaybeBorrowed<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            MaybeBorrowed::Owned(x) => &x,
+            MaybeBorrowed::Borrowed(x) => x,
+        }
+    }
+}
+
+impl<K: Eq + Hash, V, R> DefaultedRef<'_, K, V, R> {
+    pub fn get(&self, k: &K) -> MaybeBorrowed<HashMap<V, R>> {
+        match self.0.get(k) {
+            None => MaybeBorrowed::Owned(HashMap::new()),
+            Some(v) => MaybeBorrowed::Borrowed(v),
+        }
+    }
+}
+
+impl<'a, K, V, R> IntoIterator for &'a DefaultedRef<'_, K, V, R> {
+    type Item = (&'a K, &'a HashMap<V, R>);
+    type IntoIter = hash_map::Iter<'a, K, HashMap<V, R>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
+
+pub struct SingletonMap<K, V>(ReadMapMapRef<K, V>);
+
+impl<K, V> ReadMapMapRef<K, V> {
+    pub fn singleton_map(self) -> SingletonMap<K, V> {
+        SingletonMap(self)
+    }
+}
+
+impl<K, V> SingletonMap<K, V> {
+    pub fn read<'a>(&'a self, context: &'a ContextOutput) -> SingletonMapRef<'a, K, V> {
+        SingletonMapRef(self.0.read(context))
+    }
+}
+
+impl<K: Eq + Hash, V> SingletonMapRef<'_, K, V> {
+    pub fn get(&self, k: &K) -> Option<&V> {
+        self.0.get(k).map(get_singleton)
+    }
+}
+
+fn get_singleton<K>(vmap: &HashMap<K, isize>) -> &K {
+    match vmap.iter().next() {
+        Some((x, &r)) => {
+            if vmap.iter().next().is_some() {
+                panic!("Too many elements")
+            } else if r != 1 {
+                panic!("Bad count")
+            } else {
+                x
+            }
+        }
+        None => panic!("Empty map"),
+    }
+}
+
+impl<'a, K, V> IntoIterator for &'a SingletonMapRef<'_, K, V> {
+    type Item = (&'a K, &'a V);
+    type IntoIter = SingletonMapIter<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        SingletonMapIter(self.0.iter())
+    }
+}
+
+pub struct SingletonMapIter<'a, K, V>(hash_map::Iter<'a, K, HashMap<V, isize>>);
+
+impl<'a, K, V> Iterator for SingletonMapIter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, vmap)| (k, get_singleton(vmap)))
+    }
+}
+
+pub struct SingletonMapRef<'a, K, V>(ReadRefRef<'a, HashMap<K, HashMap<V, isize>>, (K, V)>);
 
 pub type ReadOrderedMapRef<K, V, R = isize> = ReadRef<BTreeMap<K, HashMap<V, R>>, (K, V), R>;
 
